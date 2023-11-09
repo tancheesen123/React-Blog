ab64a07f1dc1cefa4199dbf882262715d76aa808
cheesen <tancheesen123@Hotmail.com>
2023-11-09 15:10:51 +0800

    Tutorial 8 - 14

---
+++ src/Component/BlogList.js
@@ -0,0 +1,25 @@
+const BlogList = ({ 
+    blogs, 
+    title, 
+    handleDelete
+}) => {
+  // console.log("This is props", props);
+  //   const blogs = props.blogs;
+  //   const title = props.title;
+  //   const handleDelete= props.handleDelete;
+
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2> {blog.title} </h2>
+          <p>Written By {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,22 +1,56 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  // let name = 'mario';
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  // const [age,setAge] = useState(2523);
+  // const handleClick =() =>{
+  //     console.log(name);s
+  //     setName('luigi');
 
-    const handleClick =() =>{
-        console.log("clicking me !!!");
-    }
+  // }
 
-    const handleClickAgian = (name,e ) => {
-        console.log('hello' + name,e.target);
-    }
+  // const handleClickAgian = (name,e ) => {
+  //     console.log('hello' + name,e.target);
+  // }
 
-    return (
-      <div className="home">
-        <h2>Homepage</h2>
+  const handleDelete = (id ) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    // filter blog
+    setBlogs(newBlogs);
+    // render new blogs
+}
+
+  useEffect(() =>{
+    console.log("useEffect trigged");
+    console.log(blogs);
+  });
+
+  return (
+    <div className="home">
+      {/* <h2>Homepage</h2>
+        <p>{name} is {age} years old </p>
         <button onClick ={handleClick}>Click Me</button>
         <button onClick ={(e) => 
             handleClickAgian("mario",e)
-            }>Click Me Agian</button>
-      </div>
-    );
-  }
-   
-  export default Home;
+            }>Click Me Agian</button> */}
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+      {/* <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario Blogs!"
+      /> */}
+    </div>
+  );
+};
+
+export default Home;
